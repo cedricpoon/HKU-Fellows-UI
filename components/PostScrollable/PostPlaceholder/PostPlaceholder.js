9adff68984750e4385c2847c93f4330f6c41691c
Cedric Poon <cedric@poons.me>
2018-11-28 16:48:07 +0800

    FadeIn posts

--- components/PostScrollable/PostPlaceholder/PostPlaceholder.js
+++ components/PostScrollable/PostPlaceholder/PostPlaceholder.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { Body, Text, Icon, View, Button } from 'native-base';
+import { Body as NBBody, Text, Icon, View, Button } from 'native-base';
+import * as Animatable from 'react-native-animatable';
 
 import styles from './Styles';
 import peerStyles from '../PostPreview/Styles';
+import { FADE_IN_DURATION } from '../PostScrollable';
+
+const Body = Animatable.createAnimatableComponent(NBBody);
 
 class PostPlaceholder extends React.Component {
   render() {
     const { headline, subHeadline, icon, headlineColor, button } = this.props;
 
     return (
-      <Body style={styles.placeholder}>
+      <Body style={styles.placeholder} animation="fadeIn" duration={FADE_IN_DURATION}>
         <Text style={{color: headlineColor}}>{headline}</Text>
         {subHeadline && (
           <View style={styles.subHeadline}>
--- components/PostScrollable/PostScrollable.js
+++ components/PostScrollable/PostScrollable.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-import { FlatList } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import { FlatList as RNFlatList } from 'react-native';
 
 import Post from './PostPreview/PostPreview';
+
+const FlatList = Animatable.createAnimatableComponent(RNFlatList);
 
 class PostScrollable extends Component {
 
@@ -24,6 +27,8 @@ class PostScrollable extends Component {
           data={posts}
           keyExtractor={item => item.id}
           renderItem={this._renderPost}
+          animation='fadeIn'
+          duration={FADE_IN_DURATION}
           {...restProps}
         />
       );
@@ -36,5 +41,7 @@ class PostScrollable extends Component {
 PostScrollable.propTypes = {
   posts: PropTypes.array.isRequired
 };
+
+export const FADE_IN_DURATION = 250;
 
 export default PostScrollable;

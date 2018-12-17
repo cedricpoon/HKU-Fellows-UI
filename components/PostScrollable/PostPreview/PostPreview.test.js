770e0db1ce19894246fa373a16aee43c59b41ef3
Cedric Poon <cedric@poons.me>
2018-12-18 04:19:17 +0800

    Revert "Update PostPreview.test.js"
    This reverts commit db0d4ad50785955bbf8d74ed1d061f2c0392afe8.

--- components/PostScrollable/PostPreview/PostPreview.test.js
+++ components/PostScrollable/PostPreview/PostPreview.test.js
@@ -5,42 +5,42 @@ import Post from './PostPreview';
 describe('Testing PostPreview Component', () => {
   it('renders as expected with required props', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} />
+      <Post native timestamp={''} replyNo={0} title={''} />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as expected with required props and solved', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} solved />
+      <Post native timestamp={''} replyNo={0} title={''} solved />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as expected with required props and hashtags', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} primaryHashtag={''} secondaryHashtag={''} />
+      <Post native timestamp={''} replyNo={0} title={''} primaryHashtag={''} secondaryHashtag={''} />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as expected with required props and subtitle', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} subTitle={''} />
+      <Post native timestamp={''} replyNo={0} title={''} subTitle={''} />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as expected with required props and temperature', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} temperature={0} />
+      <Post native timestamp={''} replyNo={0} title={''} temperature={0} />
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders as expected with required props and viewed', () => {
     const wrapper = shallow(
-      <Post native timestamp={'2016-09-07'} replyNo={0} title={''} viewed />
+      <Post native timestamp={''} replyNo={0} title={''} viewed />
     );
     expect(wrapper).toMatchSnapshot();
   });

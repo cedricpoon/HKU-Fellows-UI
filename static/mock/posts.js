a2b76e158275b1f36bd16eb39150a69dc23f325e
Cedric Poon <cedric@poons.me>
2019-02-01 03:41:22 +0800

    post details context selectable

--- static/mock/posts.js
+++ static/mock/posts.js
@@ -1,188 +1,42 @@
 export default [
   {
     "id": "5c448f33bb8ea03c926d544a",
-    "author": "Rowe",
-    "timestamp": "Thu Sep 05 2013 03:05:38 GMT+0800 (Hong Kong Standard Time)",
+    "timestamp": "Thu Jan 05 2019 15:05:38 GMT+0800 (Hong Kong Standard Time)",
     "temperature": 10,
-    "content": "Minim exercitation aute non incididunt occaecat ex et culpa quis nisi cupidatat sit exercitation in. Culpa tempor aute tempor esse excepteur labore commodo qui Lorem consectetur commodo. Cillum ullamco sunt pariatur et aliquip aliqua nulla ut dolor. Id sunt incididunt irure id eiusmod sit do incididunt velit fugiat. Velit veniam officia cupidatat duis tempor cillum commodo non eiusmod aliquip ad sint ad.\r\nEsse est occaecat consectetur pariatur incididunt cupidatat minim sit dolor irure. Velit occaecat elit ipsum officia adipisicing nisi duis. Do aliqua laborum nisi nostrud consectetur labore non anim duis minim esse sit officia. Aute elit quis amet Lorem esse amet elit est veniam. Amet dolore ea ea elit pariatur. Lorem sunt nulla nisi et in reprehenderit nostrud ea non aliquip velit dolor. Magna consequat laboris nisi elit.\r\nDolore ut reprehenderit tempor eu in commodo nisi enim pariatur et. Nostrud nulla sit ut laborum excepteur irure laborum amet eiusmod. Ullamco fugiat ipsum non nulla enim nostrud. Magna elit ea tempor eu eu voluptate enim quis voluptate.\r\nEiusmod esse excepteur ex non incididunt proident in occaecat non. Veniam culpa occaecat ullamco sint fugiat aliqua irure eiusmod labore. Deserunt ullamco occaecat nisi occaecat. Officia cillum ipsum aute Lorem enim reprehenderit labore id dolor eiusmod non labore consectetur. Mollit consectetur esse sunt commodo qui commodo et non laborum quis sit esse. Ea sint in ad laborum adipisicing ea nostrud ut. Voluptate labore dolore consectetur irure Lorem amet dolore do qui adipisicing.\r\nSunt velit duis reprehenderit laboris ut ex culpa voluptate amet est ex veniam nostrud. Ad exercitation cillum aliqua tempor amet velit ullamco est cupidatat deserunt excepteur mollit. Voluptate culpa incididunt dolor officia minim eiusmod consectetur eu elit minim. Enim nostrud exercitation aliquip labore mollit ullamco consequat sit nostrud aliquip officia magna. Reprehenderit velit est commodo ex in ad occaecat aute enim labore nulla sit proident sit. In ipsum esse adipisicing proident nulla culpa et voluptate est. Nulla tempor tempor sint laboris deserunt laboris ea sit voluptate ad reprehenderit duis.\r\n"
+    "content": `According to the lecture content, I feel like they are similar in real life practice. How to distinguish them from their features in the case of Hong Kong? Why are the people so unsatisfied with them in Hong Kong?`
   },
   {
     "id": "227445a988500528d7826c6921d2e3b4a79ccf3a94cc3bcf7b667e3ae4990b36",
-    "author": "Ford",
-    "temperature": -5,
-    "timestamp": "Sat Jul 30 1983 22:16:17 GMT+0800 (Hong Kong Standard Time)",
-    "content": "Ut aliqua nostrud labore occaecat. Aute qui incididunt id esse deserunt sint occaecat occaecat id quis magna cupidatat. Dolor excepteur laborum irure excepteur.\r\nQui do qui sunt sit consectetur. Fugiat commodo proident incididunt dolor in ipsum ipsum dolor aliquip id. Id nulla adipisicing anim labore dolore Lorem quis est.\r\nVoluptate dolore consectetur commodo aliqua ea minim ex deserunt commodo esse eu ad sit excepteur. Incididunt nostrud sunt sint nisi pariatur duis aliquip laborum amet ea dolor cillum consectetur. Elit cillum cupidatat amet consequat occaecat id aliqua et ut ut ut occaecat nulla ut. Labore quis dolor sit deserunt amet esse consectetur laboris minim deserunt in nulla commodo. Magna do minim in consequat voluptate magna reprehenderit amet esse cillum nisi qui elit adipisicing. Ut veniam est anim laboris Lorem commodo ex sit qui sit amet ex non. Proident in proident ipsum irure excepteur duis labore esse enim exercitation laboris.\r\nDeserunt quis velit fugiat ea aliquip laborum do. Nulla deserunt laborum est ut reprehenderit minim. Reprehenderit quis adipisicing nostrud nisi amet dolore excepteur proident pariatur sit ex proident aute. Reprehenderit aliqua ipsum incididunt duis. Sint dolor incididunt ad excepteur ex est duis. Consequat minim culpa dolor est aliqua nisi commodo ex sint ipsum do. Anim veniam tempor incididunt mollit eiusmod excepteur in voluptate ad pariatur.\r\nEiusmod velit duis deserunt duis ut ad labore. In esse dolor sunt est culpa eiusmod officia velit deserunt anim eu tempor. Duis aliqua nulla do officia. Cillum proident velit anim aute cupidatat. Tempor anim magna culpa aute. Cupidatat ad nisi voluptate eu do mollit aute tempor et qui ut esse elit.\r\n"
-  },
-  {
-    "id": "5c448f33fa0a35cc3773e369",
-    "timestamp": "Fri Feb 04 2011 13:54:37 GMT+0800 (Hong Kong Standard Time)",
-    "content": `
-__Advertisement :)__
+    "author": "Whtcher",
+    "temperature": 28,
+    "timestamp": "Thu Jan 05 2019 17:05:38 GMT+0800 (Hong Kong Standard Time)",
+    "content": `Eminent domain is **the process of government acquiring property or land with compensation to the property owners**. They can only take it for **public purposes**, such as infrastructures. The government will have to compensate the property owners in fair market value. In Hong Kong, the Basic Law protects the right of property owners in such a capitalist city. According to [Chapter 5 of the Basic Law in Hong Kong](https://www.basiclaw.gov.hk/pda/en/basiclawtext/chapter_5.html)
 
-- __[pica](https://nodeca.github.io/pica/demo/)__ - high quality and fast image
-  resize in browser.
-- __[babelfish](https://github.com/nodeca/babelfish/)__ - developer friendly
-  i18n with plurals support and easy syntax.
+    >Article 105
+    >The Hong Kong Special Administrative Region shall, in accordance with law, protect the right of individuals and legal persons to the acquisition, use, disposal and inheritance of property and their right to compensation for lawful deprivation of their property.
+    >Such compensation shall correspond to the real value of the property concerned at the time and shall be freely convertible and paid without undue delay.
+    >The ownership of enterprises and the investments from outside the Region shall be protected by law.
 
-You will like those projects!
+    Protected by this law, if the government fails to compensate the owners, the owners have the right to sue the government.
 
----
-
-# h1 Heading 8-)
-## h2 Heading
-### h3 Heading
-#### h4 Heading
-##### h5 Heading
-###### h6 Heading
-
-
-## Horizontal Rules
-
-___
-
----
-
-***
-
-
-## Typographic replacements
-
-Enable typographer option to see result.
-
-(c) (C) (r) (R) (tm) (TM) (p) (P) +-
-
-test.. test... test..... test?..... test!....
-
-!!!!!! ???? ,,  -- ---
-
-"Smartypants, double quotes" and 'single quotes'
+    On the other hand, zoning is a process which **the government restricts the use of land based on where the land is located**. It is simply a public control over the private properties. No compensation is required to be given to property owners. Property owners are not happy with such practice as they will face a loss of financial interest. Yet,    it is very popular among the local government planners as it doesn’t cost the govt any besides the consultation fee of zoning.
     `
   },
   {
-    "id": "5c448f334fcf12a88c4fd0f1",
-    "author": "Ferguson",
-    "timestamp": "Wed Jan 12 2011 21:31:56 GMT+0800 (Hong Kong Standard Time)",
+    "id": "5c448f33bb8ea03c926d544b",
+    "author": "barnettp",
+    "timestamp": "Thu Jan 05 2019 18:05:38 GMT+0800 (Hong Kong Standard Time)",
     "temperature": 0,
-    "content": `
-## Emphasis
-
-**This is bold text**
-
-__This is bold text__
-
-*This is italic text*
-
-_This is italic text_
-
-~~Strikethrough~~
-
-
-## Blockquotes
-
-> Blockquotes can also be nested...
->> ...by using additional greater-than signs right next to each other...
-> > > ...or with spaces between arrows.
-
-## Lists
-
-Unordered
-
-+ Create a list by starting a line with \`+\`, \`-\`, or \`*\`
-+ Sub-lists are made by indenting 2 spaces:
-  - Marker character change forces new list start:
-    * Ac tristique libero volutpat at
-    + Facilisis in pretium nisl aliquet
-    - Nulla volutpat aliquam velit
-+ Very easy!
-
-Ordered
-
-1. Lorem ipsum dolor sit amet
-2. Consectetur adipiscing elit
-3. Integer molestie lorem at massa
-
-
-1. You can use sequential numbers...
-1. ...or keep all the numbers as \`1.\`
-
-Start numbering with offset:
-
-57. foo
-1. bar
-    `
+    "content": `Zoning aims to protect public interest by imposing limitations to the existing lands or properties, while eminent domain is being used to buy over the properties for public purposes projects. But I am also not sure about why people are unsatisfied with them in Hong Kong as they are supposed to be carried out for the public interest.`
   },
   {
-    "id": "5c448f3390966b8261eff42b",
-    "author": "Ida",
-    "temperature": 20,
-    "timestamp": "Fri May 11 1984 09:08:52 GMT+0800 (Hong Kong Standard Time)",
-    "content": `
-## Code
+    "id": "5c448f33bb8ea03c926d544c",
+    "author": "johntang",
+    "timestamp": "Thu Jan 06 2019 18:05:38 GMT+0800 (Hong Kong Standard Time)",
+    "temperature": 5,
+    "content": `They are different in terms of the compensation paid to the land owners who are affected by the practice, as well as the ownership of the property.
 
-Inline \`code\`
-
-Indented code
-
-    // Some comments
-    line 1 of code
-    line 2 of code
-    line 3 of code
-
-
-Block code "fences"
-
-\`\`\`
-Sample text here...
-\`\`\`
-
-Syntax highlighting
-
-\`\`\` js
-var foo = function (bar) {
-  return bar++;
-};
-
-console.log(foo(5));
-\`\`\`
-
-## Tables
-
-| Option | Description |
-| ------ | ----------- |
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-Right aligned columns
-
-| Option | Description |
-| ------:| -----------:|
-| data   | path to data files to supply the data that will be passed into templates. |
-| engine | engine to be used for processing templates. Handlebars is the default. |
-| ext    | extension to be used for dest files. |
-
-
-## Links
-
-[link text](http://dev.nodeca.com)
-
-[link with title](http://nodeca.github.io/pica/demo/ "title text!")
-
-Autoconverted link https://github.com/nodeca/pica (enable linkify to see)
-
-
-## Images
-
-![Minion](https://octodex.github.com/images/minion.png)
-![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
-
-Like links, Images also have a footnote style syntax
-
-![Alt text][id]
-
-With a reference later in the document defining the URL location:
-
-[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+    When the government carries out eminent domain, they are also required to compensate the land owners who are being affected. When talking about zoning, it is just a planning practice which limits the use of land in the certain area. **For example, a factory is not allowed to be built next to residential area.** It is the right to regulate the private parties for the sake of public interest.
     `
-  }
+  },
 ]

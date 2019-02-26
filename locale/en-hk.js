d17e150cc0d8e4bc0e403fd89cc30c3f646172d4
Cedric Poon <cedric@poons.me>
2019-02-26 19:58:28 +0800

    update moodle no "#"

--- locale/en-hk.js
+++ locale/en-hk.js
@@ -1,5 +1,5 @@
 export default {
-  'post.moodle': '#Moodle',
+  'post.moodle': 'Moodle',
   'post.noPostTitle': 'No Result / No Posts Yet',
   'post.noPostContent': 'Feel free to raise the first post',
   'post.noPostButton': 'Compose',

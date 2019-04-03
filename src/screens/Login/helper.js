8a969ccac877d77b2f180ac03c7422619cee8203
Cedric Poon <cedric@poons.me>
2019-04-03 18:11:26 +0800

    update test scenarios

--- src/screens/Login/helper.js
+++ src/screens/Login/helper.js
@@ -1,8 +1,8 @@
-import firebase from 'react-native-firebase';
-
 export async function retrieveToken() {
   // Get the current token if it exists
   try {
+    const firebase = require('react-native-firebase');
+
     const fcmToken = await firebase.messaging().getToken();
     return fcmToken
   } catch (e) {

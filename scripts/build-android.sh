be66c19963f7ce5cd371e694f0dd1f7c22acaf98
Cedric Poon <cedric@poons.me>
2019-04-10 03:28:07 +0800

    npm run build

--- scripts/build-android.sh
+++ scripts/build-android.sh
@@ -1,3 +1,4 @@
 cd android && ./gradlew assembleRelease
 cd .. && mkdir -p build/
 mv ./android/app/build/outputs/apk/release/*.apk ./build/
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ./build/*.keystore ./build/*.apk hkuf
